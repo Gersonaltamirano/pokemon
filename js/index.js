644c3b533caff6f2bc09361949eecf10d56a5d58
Gerson Altamirano <gersonaltamirano@gmail.com>
2022-05-28 00:39:12 -0600

    mejorando la interface

--- js/index.js
+++ js/index.js
@@ -5,6 +5,8 @@ const ul = document.createElement('ul');
 //elementos del primero pokemon
 const div1 = document.createElement('div');
 div1.classList.add('card', 'col-md-4');
+div1.style.width = '13rem';
+div1.style.height = '25rem';
 const img1 = document.createElement('img');
 img1.classList.add('card-img-top');
 const div3 = document.createElement('div');
@@ -13,9 +15,11 @@ const parrafo1 = document.createElement('p');
 
 //elementos del segundo pokemon
 const div2 = document.createElement('div');
-div2.classList.add('card', 'col-md-4');
+div2.classList.add('card', 'col-md-4', );
+div2.style.width = '13rem';
+div2.style.height = '25rem';
 const img2 = document.createElement('img');
-img2.classList.add('card-img-top');
+img2.classList.add('card-img-top', );
 const div4 = document.createElement('div');
 div4.classList.add('card-body');
 const parrafo2 = document.createElement('p');
@@ -40,17 +44,22 @@ fetch(`${API_URL}${id_aleatorio1}`)
         let poder_ataque =  Math.floor(Math.random() * (500 - 100)) + 100
         let SP = pokemon.stats[0].base_stat;
         let DP = pokemon.stats[2].base_stat;
-    
 
         img1.src = pokemon.sprites.other.dream_world.front_default;
         div1.appendChild(img1);
         div1.appendChild(div3);
         parrafo1.appendChild(
-            document.createTextNode('Nombre: '+ pokemon.name + '\n' + 'Tipo: '+pokemon.types[0].type.name + '\n' + 'Peso: '+pokemon.weight + '\n' + 'Altura: '+pokemon.height + '\n' + 'Ataque: '+ nombre_ataque + '\n' + 'Poder: '+poder_ataque  + '\n' + 'SP: '+SP + '\n' + 'DP: '+DP)
+            document.createTextNode(
+                'Nombre: '+ pokemon.name 
+                + '\n' + 'Tipo: '+pokemon.types[0].type.name 
+                + '\n' + 'Peso: '+pokemon.weight 
+                + '\n' + 'Altura: '+pokemon.height 
+                + '\n' + 'Ataque: '+ nombre_ataque 
+                + '\n' + 'Poder: '+poder_ataque  
+                + '\n' + 'SP: '+SP 
+                + '\n' + 'DP: '+DP)
         );
         div1.appendChild(parrafo1);
-
-        
     })
 
 //funcion para obtener el pokemon2
@@ -67,12 +76,20 @@ fetch(`${API_URL}${id_aleatorio2}`)
         let SP = pokemon.stats[0].base_stat;
         let DP = pokemon.stats[2].base_stat;
  
-
         img2.src = pokemon.sprites.other.dream_world.front_default;
         div2.appendChild(img2);
         div2.appendChild(div4);
         parrafo2.appendChild(
-            document.createTextNode('Nombre: '+ pokemon.name + '\n' + 'Tipo: '+pokemon.types[0].type.name + '\n' + 'Peso: '+pokemon.weight + '\n' + 'Altura: '+pokemon.height + '\n' + 'Ataque: '+ nombre_ataque + '\n' + 'Poder: '+poder_ataque + '\n' + 'SP: '+SP + '\n' + 'DP: '+DP)
+            document.createTextNode(
+                'Nombre: '+ pokemon.name 
+                + '\n' + 'Tipo: ' + pokemon.types[0].type.name 
+                + '\n' + 'Peso: ' + pokemon.weight 
+                + '\n' + 'Altura: ' + pokemon.height 
+                + '\n' + 'Ataque: ' + nombre_ataque 
+                + '\n' + 'Poder: ' + poder_ataque 
+                + '\n' + 'SP: ' + SP 
+                + '\n' + 'DP: ' + DP
+            )
         );
         div2.appendChild(parrafo2);
         
